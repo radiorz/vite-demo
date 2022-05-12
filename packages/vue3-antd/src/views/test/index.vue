a7616db60ad2bee278eed824285fc15d96c85226
tikkhun <s565338107@163.com>
2022-05-12 11:17:46 +0800

    feat: 项目都可以跑

---
+++ packages/vue3-antd/src/views/test/index.vue
@@ -0,0 +1,33 @@
+<!--
+* @FileDescription: 该文件的描述信息
+* @Date: 2022年05月12日 11:05:09
+* TODO:
+* [] 
+-->
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "TheTest",
+  // components: {},
+  props: {},
+  // 从组件触发 emits 事件
+  // emits:{},
+  // 暴露
+  // expose: {},
+  data() {
+    return {};
+  },
+  // computed:{},
+  // watch:{},
+  // create(){},
+  // methods:{}
+});
+</script>
+
+<template>
+  <div class="" />
+  <span>123</span>
+</template>
+
+<style lang="scss" scoped></style>
--- packages/vue3-antd/vite.config.ts
+++ packages/vue3-antd/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5000,
+  },
+  alias: {
+    "@": resolve(__dirname, "./src"),
+  },
   plugins: [vue()],
 });
