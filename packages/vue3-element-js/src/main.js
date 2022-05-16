74a096cb34eea1648e65167bbe59a0bcbe0f8ec8
tikkhun <s565338107@163.com>
2022-05-16 16:46:00 +0800

    feat(vue3-element-js): 试图组织css 代码

--- packages/vue3-element-js/src/main.js
+++ packages/vue3-element-js/src/main.js
@@ -5,10 +5,10 @@ import App from "./App.vue";
 import router from "./router";
 import i18n from "@/plugins/i18n";
 
+// import "@/style/main.css"; // global css
 import "@/style/index.scss"; // global css
 // app.js
 import { createPinia } from "pinia";
-import "@/style/main.css";
 const app = createApp(App);
 appInject(app);
 app.use(i18n);
