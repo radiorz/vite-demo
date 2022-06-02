3b53d29278aeb9e96818edaad0a25a65c3f1681c
tikkhun <s565338107@163.com>
2022-06-02 16:17:24 +0800

    feat(vue3-vuetify): 使用 vue-cli 添加 vuetify

--- packages/vue3-vuetify/vite.config.ts
+++ packages/vue3-vuetify/vite.config.ts
@@ -2,13 +2,18 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 
+// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+import vuetify from "vite-plugin-vuetify";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 5000,
   },
-  alias: {
-    "@": resolve(__dirname, "./src"),
+  resolve: {
+    alias: {
+      "~": resolve(__dirname, "./src"),
+    },
   },
-  plugins: [vue()],
+  plugins: [vue(), vuetify({ autoImport: true })],
 });
