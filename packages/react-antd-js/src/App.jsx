476a33f58c5154ed201262c8845edf3433956cb7
tikkhun <s565338107@163.com>
2022-06-02 16:07:42 +0800

    feat(react-antd-js): 尝试使用vite-plugin-pages

--- packages/react-antd-js/src/App.jsx
+++ packages/react-antd-js/src/App.jsx
@@ -12,6 +12,12 @@ import About from "./views/About";
 import NotFound from "./views/NotFound";
 import routes from "~react-pages";
 console.log("routes", routes);
+const NestLayout = ({ children, i }) => (
+  <>
+    {i}
+    {children}
+  </>
+);
 function App() {
   return (
     <>
@@ -36,8 +42,19 @@ function App() {
           {/* 更多路由 */}
           <Route path="/about" element={<About />} />
           <Route path="/404" element={<NotFound></NotFound>} />
+          {/* 嵌套式的路由 */}
+          <Route path="/nest" element={<NestLayout i={1} />}>
+            <Route path="nest1" element={<div>121121212121</div>} />
+            <Route path="nest" element={<NestLayout i={2} />}>
+              <Route path="nest" element={<NestLayout i={3} />}>
+                <Route path="nest" element={<div>4</div>} />
+              </Route>
+            </Route>
+          </Route>
         </Routes>
+        {/* 测试的路由 */}
         {useRoutes(routes)}
+        {/* {process.env === "production" ? null : useRoutes(routes)} */}
       </Suspense>
     </>
   );
---
+++ packages/react-antd-js/src/test/views/About/[...all].jsx
@@ -0,0 +1,3 @@
+export default () => {
+  return <p>about ...all routes</p>;
+};
