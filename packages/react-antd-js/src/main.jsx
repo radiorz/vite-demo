0a9de38d2ab8bc7ca5549b9be98602e84a3acb2a
tikkhun <s565338107@163.com>
2023-02-14 15:16:23 +0800

    chore: use es6 method

--- packages/react-antd-js/src/main.jsx
+++ packages/react-antd-js/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { store } from "./app/store";
 import App from "./app";
 import { Provider } from "react-redux";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
